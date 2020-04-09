be34c2f471b0d311d9c6bcfecdbdb356a2028a1f
Bixbeat <ahlevering@hotmail.com>
2020-04-09 11:19:45 +0200

    Added support for querying pano locations

--- lib/data_mgmt.py
+++ lib/data_mgmt.py
@@ -1,46 +1,54 @@
 import csv
 from datetime import datetime
 
-def group_by_anchors(records):
-    """Groups panoramas by their anchor location.
-    Records must be sorted by anchor XY!
+def group_by_xy(records, x_col, y_col, precision=-1):
+    """Groups panoramas by their xy location.
+    Records must be sorted by XY columns of interest!
     
     Arguments:
-        records {List of lists} -- Contains records extracted from a Streetview database 
+        records {List of lists} -- Contains records extracted from a Streetview database
+        x_col {int} -- Index of x-coordinate column
+        y_col {int} -- Index of y-coordinate column
+
+    Keyword Arguments:
+        precision {int} -- Number of digits used for grouping. -1 is all digits. (default: {-1})
     
     Returns:
         dict -- Contains records grouped by their anchor location as dict key
     """    
     groups = {}
-    records_at_anchor = []
+    records_at_xy = []
     records = iter(records)
 
     # Initialize t-1
     prev_record = next(records)
-    records_at_anchor.append(prev_record)
+    records_at_xy.append(prev_record)
 
     for record in records:
-        # Check if using same anchor point
-        if record[3:5] != prev_record[3:5]:
-            groups[f'{prev_record[3]}-{prev_record[4]}'] = records_at_anchor
-            records_at_anchor = []     
-        records_at_anchor.append(record)
+        # Check if panos are close enough to be considered at the same location
+        if str(record[x_col])[:precision] != str(prev_record[x_col])[:precision]:
+            if str(record[y_col])[:precision] != str(prev_record[y_col])[:precision]:
+                groups[f'{prev_record[x_col]}-{prev_record[y_col]}'] = records_at_xy
+                records_at_xy = []     
+        records_at_xy.append(record)
         prev_record = record
     return groups
 
-def get_anchor_timestats(anchor_records):
+def get_xy_timestats(xy_records, x_col, y_col):
     """Calculates biggest timedif, min pano date, max pano date, and num. of distinct pano dates per anchor XY
     
     Arguments:
-        anchor_records {dict} -- Dictionary with anchor xy as keys and records at anchor xy as entries
+        anchor_records {dict} -- Dictionary with xy as keys and records at xy as entries
+        x_col {int} -- Index of x-coordinate column
+        y_col {int} -- Index of y-coordinate column        
     
     Returns:
-        list -- List containing the anchor xy and its biggest time difference
+        list -- List containing the xy and its biggest time difference
     """    
-    anchors = []
+    xy_stats = []
 
-    for a in anchor_records:
-        timestamps = [datetime.strptime(entry[2], '%Y-%m') for entry in anchor_records[a]]
+    for a in xy_records:
+        timestamps = [datetime.strptime(entry[2], '%Y-%m') for entry in xy_records[a]]
         min_time = min(timestamps)
         max_time = max(timestamps)
         num_timesteps = len(set(timestamps))
@@ -48,15 +56,15 @@ def get_anchor_timestats(anchor_records):
         month_timediff = 12 * (max_time.year - min_time.year) + (max_time.month - min_time.month)
         year_timediff = month_timediff/12
 
-        anchors.append([anchor_records[a][0][0],
-                        anchor_records[a][0][3],
-                        anchor_records[a][0][4],
+        xy_stats.append([xy_records[a][0][0],
+                        xy_records[a][0][x_col],
+                        xy_records[a][0][y_col],
                         min_time,
                         max_time,
                         month_timediff,
                         year_timediff,
                         num_timesteps])
-    return anchors
+    return xy_stats
 
 def export_to_csv(out_filepath, record_list, header=None):
     """Simple utility to export SQLite records to a CSV
